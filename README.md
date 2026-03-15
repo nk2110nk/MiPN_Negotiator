d0c7561dae1588dff584883d9e72a7014fbb360b
nakata <nk2110nk@icloud.com>
2026-03-15 15:58:21 +0900

    テスト3者間化実装

---
+++ README.md
@@ -0,0 +1,125 @@
+# Transformer-based Negotiation AI Agent
+## Overview
+implementation code for VeNAS and MiPN -based negotiation AI agent architecture
+## Instructions
+
+### Requirements
+
+```
+absl-py==0.13.0
+cachetools==4.2.2
+certifi==2021.5.30
+charset-normalizer==2.0.5
+click==8.0.1
+click-config-file==0.6.0
+cloudpickle==1.6.0
+colorlog==6.4.1
+configobj==5.0.6
+cycler==0.10.0
+dill==0.3.4
+future==0.18.2
+gif==3.0.0
+google-auth==1.35.0
+google-auth-oauthlib==0.4.6
+grpcio==1.40.0
+gym==0.26.2
+idna==3.2
+importlib-metadata==4.8.1
+inflect==5.3.0
+joblib==1.0.1
+kiwisolver==1.3.2
+Markdown==3.3.4
+matplotlib==3.4.3
+negmas==0.8.8
+networkx==2.6.2
+numpy==1.21.2
+oauthlib==3.1.1
+pandas==1.3.2
+Pillow==7.2.0
+progressbar2==3.53.2
+protobuf==3.17.3
+psutil==5.8.0
+py4j==0.10.9.2
+pyasn1==0.4.8
+pyasn1-modules==0.2.8
+pyglet==1.5.0
+pyparsing==2.4.7
+pytest-runner==5.3.1
+python-dateutil==2.8.2
+python-utils==2.5.6
+pytz==2021.1
+PyYAML==5.4.1
+requests==2.26.0
+requests-oauthlib==1.3.0
+rsa==4.7.2
+scikit-learn==0.24.2
+scipy==1.7.1
+seaborn==0.11.2
+six==1.16.0
+sklearn==0.0
+stable-baselines3==1.2.0
+stringcase==1.2.0
+tabulate==0.8.9
+tensorboard==2.6.0
+tensorboard-data-server==0.6.1
+tensorboard-plugin-wit==1.8.0
+threadpoolctl==2.2.0
+torch==1.9.0
+tqdm==4.62.2
+typing==3.7.4.3
+typing-extensions==3.10.0.2
+urllib3==1.26.6
+Werkzeug==2.0.1
+zipp==3.5.0
+
+```
+
+
+### Running experiments
+
+- Example command for training:
+```
+python3 ./train.py -a Boulware Conceder Linear TitForTat1 TitForTat2 -i Laptop ItexvsCypress IS_BT_Acquisition Grocery thompson Car EnergySmall_A
+```
+
+
+- Example command for testing with a pretrained model:
+```
+python3 ./test_negotiator.py -a Boulware Conceder Linear TitForTat1 TitForTat2 -i Laptop ItexvsCypress IS_BT_Acquisition Grocery thompson Car EnergySmall_A -m ./results/260311-034347/MiPN
+```
+
+python3 ./test_negotiator.py -a Boulware -i Laptop -m ./results/260311-034347/MiPN/
+
+- `-a` and `-i` arguments specify agents and issues for training or testing
+
+## 各ファイル・クラスの説明
+### ./train.py
+- 学習実行
+### ./test_negotiator.py
+- テスト実行
+### ./ppo_scratch.py
+- 学習アルゴリズム実装部
+- `PPO` : 環境を切り替えながら学習ループを回す．ロールアウトを実行し集めたデータで勾配更新の流れ
+- stable-baselines3の実装をベースにAI agent用に改良（複数環境の切り替えやTransformer用の拡張）
+### ./policy.py
+- 各コンポーネントをまとめて全体のモデルにしている部分．ロールアウトバッファに関する定義もここにある
+- `Transformer_Policy` : モデル本体．Transformer，方策・価値ネットワークなどを備え順伝搬等の処理を記述してある
+- `RolloutBuffer` : シミュレーション時のデータを格納するためのバッファの定義&GAEの計算処理もここにある
+### ./NegTransformer.py
+- AIエージェントに用いたTransformer部分の実装．
+### ./envs/env.py
+- gymnasium環境を継承した交渉シミュレーション用環境を定義．
+- `NaiveEnv` : RL用の各種変数やドメイン読み込み，交渉セッション，報酬等の定義
+- `AOPEnv` : `step`を改良しAOP準拠の挙動を実装
+### ./envs/rl_negotiator.py
+- 学習時・テスト時のAIエージェント本体であるNegotiatorを実装
+- `RLNegotiator` : 学習時のエージェント本体．`env.py`の`step`時に選択された`self.next_bid`がそのまま相手に提案される
+- `TestRLNegotiator.py` : テスト時のエージェント本体．チェックポイントからモデルをロードしそれを用いて推論を行う．
+### ./envs/observer.py
+- bid履歴を観測するためのobserverを定義
+- `EmbeddedObserveHistroy` : OpenAIのテキスト埋め込みによる埋込ベクトルをjsonファイルからロードし，実際のbidを埋め込みバッファに格納する
+### ./embedding_model.py
+- 新規に埋め込みベクトルを作成したい場合はこのファイルを実行．
+- `MyEmbedding` : 埋め込みベクトルを新規作成 or 作成済みの埋め込みベクトルを`embeddings`に保存してあるjsonファイルからロードする
+- `self.client = openai.OpenAI(api_key='')`にapi_keyを入力
+
--- test_negotiator.py
+++ test_negotiator.py
@@ -1,11 +1,13 @@
 import gc
 import os
-import csv
+import argparse # 変更箇所
 from multiprocessing import Pool
 from itertools import product
+import csv
 
 import sao
 from negmas import UtilityFunction, Issue
+from negmas import load_genius_domain_from_folder # 変更箇所
 from sao.my_sao import MySAOMechanism
 from sao.my_negotiators import *
 from envs.rl_negotiator import TestRLNegotiator
@@ -13,41 +15,39 @@ from matplotlib import pyplot as plt
 
 ISSUE_NAMES = [
     'Laptop',
-    # 'ItexvsCypress',
-    # 'IS_BT_Acquisition',
-    # 'Grocery',
-    # 'thompson',
-    # 'Car',
-    # 'EnergySmall_A'
+    'ItexvsCypress',
+    'IS_BT_Acquisition',
+    'Grocery',
+    'thompson',
+    'Car',
+    'EnergySmall_A'
 ]
 AGENT_LIST = [
     'Boulware',
-    # 'Linear',
-    # 'Conceder',
-    # 'TitForTat1',
-    # 'TitForTat2',
-    # "AgentK",
-    # "HardHeaded",
-    # "Atlas3",
-    # "AgentGG",
+    'Linear',
+    'Conceder',
+    'TitForTat1',
+    'TitForTat2',
+    "AgentK",
+    "HardHeaded",
+    "Atlas3",
+    "AgentGG",
 ]
-# LOAD_PATH = "./results/211025-105809/"  # issue
-LOAD_PATH = "./results/260311-034347/MiPN/"  # venas
-PLOT = False
-
 
 def a(x):
     return 'T' if x else 'F'
 
 
-def run_session(path, save_path,  opponent, domain, util1, util2, det, noise):
+def run_session_trained(path, save_path, opponent, domain, util1, util2, util3, det, noise):
     session = MySAOMechanism(issues=domain, n_steps=80, avoid_ultimatum=False)
     my_agent = TestRLNegotiator(domain, path, deterministic=det, mode='issue')
-    opponent = get_opponent(opponent, add_noise=noise)
+    opponent0 = get_opponent(opponent[0], add_noise=noise) # 変更箇所
+    opponent1 = get_opponent(opponent[1], add_noise=noise) # 変更箇所
 
-    # 先攻想定
+    # 本実験では先攻・後攻の想定を考慮する必要がない
     session.add(my_agent, ufun=util1)
-    session.add(opponent, ufun=util2)
+    session.add(opponent0, ufun=util2) # 変更箇所
+    session.add(opponent1, ufun=util3) # 変更箇所
 
     result = session.run()
 
@@ -60,21 +60,22 @@ def run_session(path, save_path,  opponent, domain, util1, util2, det, noise):
         plt.close()
 
     session.reset()
-    del my_agent, util1._ami, util2._ami, session, opponent
+    del my_agent, session, opponent0, opponent1
     gc.collect()
 
     if result['agreement'] is not None:
-        my_util, opp_util = util1(result['agreement']), util2(result['agreement'])
+        my_util, opp_util1, opp_util2 = util1(result['agreement']), util2(result['agreement']), util3(result['agreement']) # 変更箇所
     else:
-        my_util, opp_util = 0, 0
+        my_util, opp_util1, opp_util2 = 0, 0, 0 # 変更箇所
 
     return [
         my_util,
-        opp_util,
-        my_util + opp_util,
-        my_util * opp_util,
+        opp_util1, # 変更箇所
+        opp_util2, # 変更箇所
+        my_util + opp_util1 + opp_util2, # 変更箇所
+        my_util * opp_util1 * opp_util2, # 変更箇所
         result['agreement'],
-        result['step']
+        result['step'], 
     ]
     # return {
     #     'my_util': my_util,
@@ -86,17 +87,27 @@ def run_session(path, save_path,  opponent, domain, util1, util2, det, noise):
     # }
 
 
-def test_negotiator(config):
+def test_trained(config):
     issue, agent, det, noise, save_path = config
-    results = [['my_util', 'opp_util', 'social', 'nash', 'agreement', 'step']]
+    
+    results = [['my_util', 'opp_util1', 'opp_util2', 'social', 'nash', 'agreement', 'step']] # 変更箇所
+
+    # ドメイン設定
+    # !!!ここさえ変更すればOK!!!
     domain, _ = Issue.from_genius('./domain/' + issue + '/domain.xml')
-    util1, _ = UtilityFunction.from_genius('./domain/' + issue + '/utility1.xml')
-    util2, _ = UtilityFunction.from_genius('./domain/' + issue + '/utility2.xml')
+    scenario_number1 = 0
+    scenario_number2 = 1
+    scenario_number3 = 2
+    util1, _ = UtilityFunction.from_genius(f'./domain/{issue}/utility{scenario_number1+1}.xml')
+    util2, _ = UtilityFunction.from_genius(f'./domain/{issue}/utility{scenario_number2+1}.xml')
+    util3, _ = UtilityFunction.from_genius(f'./domain/{issue}/utility{scenario_number3+1}.xml')
+
+    # ここは2人用に改良しなければならない
     for _ in range(1 if PLOT else 100):
-        results.append(run_session(f'{LOAD_PATH}{issue}-{agent}-v0.zip', save_path, agent, domain, util1, util2, det, noise))
+        results.append(run_session_trained(f'{LOAD_PATH}{issue}-{agent[0]}-{agent[1]}-v0.zip', save_path, agent, domain, util1, util2, util3, det, noise))
 
     if not PLOT:
-        with open(f'{save_path}{issue}-{agent}-d{a(det)}-n{a(noise)}.tsv', 'w') as f:
+        with open(f'{save_path}{issue}-{agent[0]}-{agent[1]}-d{a(det)}-n{a(noise)}.tsv', 'w') as f: # 変更箇所
             writer = csv.writer(f, delimiter='\t')
             writer.writerows(results)
 
@@ -127,22 +138,51 @@ def get_opponent(opponent, add_noise=False):
     return opponent
 
 
-def main():
-    # try:
-    #     os.makedirs(LOAD_PATH + 'img' if PLOT else 'csv')
-    # except FileExistsError:
-    #     pass
+def main_trained():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--agents', '-a', nargs='*', required=True, type=str)
+    parser.add_argument('--issues', '-i', nargs='*', required=True, type=str)
+    parser.add_argument('--model', '-m', required=True, type=str)
+    parser.add_argument('--scale', '-s', type=str, default='small')
+    parser.add_argument('--plot', '-p', action='store_true')
+    parser.add_argument('--is_first', '-if', action='store_true')
+    args = parser.parse_args()
+    print(args)
+    
+    agents = args.agents
+    issues = args.issues
+    model_path = args.model
+    scale = args.scale
+    plot = args.plot
+    is_first = args.is_first
 
-    p = Pool(len(AGENT_LIST))
-    for det, noise in product([True, False], [False]):
-        save_path = LOAD_PATH + ('img' if PLOT else 'csv') + f'/det={det}_noise={noise}/'
-        try:
-            os.makedirs(save_path)
-        except FileExistsError:
-            exit(0)
-        for issue in ISSUE_NAMES:
-            p.map(test_negotiator, [(issue, agent, det, noise, save_path) for agent in AGENT_LIST])
+    global LOAD_PATH
+    LOAD_PATH = model_path
+
+    global PLOT
+    PLOT = plot
+    
+    if isinstance(issues, str):
+        issues = [issues]
+    if isinstance(agents, str):
+        agents = [agents]
+
+    p = Pool(len(agents))
+    agent = [None, None] # 変更箇所
+            
+# 変更箇所
+    for i in range(len(agents)):
+        agent[0] = agents[i]
+        for j in range(i, len(agents)):
+            agent[1] = agents[j]
+            for issue in issues:
+                for det, noise in product([False], [False]):
+                    save_path = LOAD_PATH + ('/img' if PLOT else '/csv') + f'/{agent[0]}-{agent[1]}/' + f'/{issue}/' + f'/det={det}_noise={noise}/'
+                    if not os.path.isdir(save_path):
+                        os.makedirs(save_path)
+                        
+                    p.map(test_trained, [(issue, agent, det, noise, save_path)])
 
 
 if __name__ == '__main__':
-    main()
+    main_trained()
